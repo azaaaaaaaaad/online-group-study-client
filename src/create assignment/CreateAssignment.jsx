50feb7b6ffde8fa0d468791f9ac251a9ac4fc732
jrazad10 <jrazad10@gmail.com>
2024-05-12 13:56:10 +0600

    create assignment form is connected with db, now anuone can post

--- src/create assignment/CreateAssignment.jsx
+++ src/create assignment/CreateAssignment.jsx
@@ -1,29 +1,56 @@
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
+import axios from "axios";
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
+import toast from "react-hot-toast";
 
 const CreateAssignment = () => {
+    const [startDate, setStartDate] = useState(new Date());
+
+    const handleForm = async e => {
+        e.preventDefault()
+        const form = e.target;
+        const title = form.title.value;
+        const description = form.description.value;
+        const marks = form.marks.value;
+        const photo = form.photo.value;
+        const difficultyLevel = form.difficultyLevel.value;
+        const date = startDate
+        const info = {title, description, marks, photo, difficultyLevel, date}
+        console.table(info);
+
+        try{
+            const {data} = await axios.post(`${import.meta.env.VITE_API_URL}/assignments`, info)
+            console.log(data);
+            toast.success('assignment created successfully')
+        }catch(error){
+            toast.error(error?.message)
+        }
+    }
+
     return (
-        <form>
+        <form onSubmit={handleForm}>
             <div className='text-center space-y-8'>
                 <div>
                     <label>Title:</label>
-                    <input className='border' type="text" name="title" required />
+                    <input className='border p-2 rounded-md' type="text" name="title" required />
                 </div>
                 <div>
                     <label>Description:</label>
-                    <input className='border'  type="text" name="description" required />
+                    <input className='border p-2 rounded-md'  type="text" name="description" required />
                 </div>
                 <div>
                     <label>Marks:</label>
-                    <input className='border' type="number" name="marks" required />
+                    <input className='border p-2 rounded-md' type="number" name="marks" required />
                 </div>
                 <div>
-                    <label>Thumbnail Image URL:</label>
-                    <input className='border' type="text" name="thumbnailUrl" required />
+                    <label>Image URL:</label>
+                    <input className='border p-2 rounded-md' type="text" name="photo" required />
                 </div>
                 <div>
                     <label>Assignment Difficulty Level:</label>
-                    <select className='border' name="difficultyLevel" required>
+                    <select className='border p-2 rounded-md' name="difficultyLevel" required>
                         <option value="Easy">Easy</option>
                         <option value="Medium">Medium</option>
                         <option value="Hard">Hard</option>
@@ -31,7 +58,7 @@ const CreateAssignment = () => {
                 </div>
                 <div>
                     <label>Due Date:</label>
-                    {/* <DatePicker selected={formData.dueDate} onChange={handleDateChange} /> */}
+                    <DatePicker className='border p-2 rounded-md'  selected={startDate} onChange={(date) => setStartDate(date)} />
                 </div>
                 <button className='btn btn-active' type="submit">Submit</button>
             </div>
