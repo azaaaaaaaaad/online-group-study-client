09a7c278fa4f4cbcaccb6b1882e2a89d7f2fb046
jrazad10 <jrazad10@gmail.com>
2024-05-14 10:18:04 +0600

    data showing for specific user in MyAttemptedAssignments page

--- src/My Attempted Assignments/MyAttemptedAssignments.jsx
+++ src/My Attempted Assignments/MyAttemptedAssignments.jsx
@@ -1,10 +1,145 @@
+import { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../provider/AuthProvider";
+import axios from "axios";
+import { data } from "autoprefixer";
 
 
 const MyAttemptedAssignments = () => {
+    const { user } = useContext(AuthContext)
+    const [submissions, setSubmissions] = useState([])
+
+    useEffect(() => {
+        const getData = async () => {
+            const { data } = await axios(`http://localhost:5000/assignments-submission/${user?.email}`)
+            setSubmissions(data)
+        }
+        getData()
+    }, [user])
+
+    console.log(submissions);
+
     return (
-        <div>
-            MyAttemptedAssignments
-        </div>
+        <section className='container px-4 mx-auto pt-12'>
+                <div className="flex items-center justify-center gap-8">
+                    <h2 className='text-2xl text-center font-bold'>My Attempted Assignments</h2>
+                    <span className='px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full '>
+                        {submissions.length} Assignment
+                    </span>
+                </div>
+            <div className='flex items-center gap-x-3'>
+            </div>
+
+            <div className='flex flex-col mt-6'>
+                <div className='-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8'>
+                    <div className='inline-block min-w-full py-2 align-middle md:px-6 lg:px-8'>
+                        <div className='overflow-hidden border border-black  md:rounded-lg'>
+                            <table className='min-w-full divide-y divide-gray-200 '>
+                                <thead className='bg-blue-100 '>
+                                    <tr>
+                                        <th
+                                            scope='col'
+                                            className='py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500'
+                                        >
+                                            <div className='flex items-center gap-x-3'>
+                                                <span>Assignment Title</span>
+                                            </div>
+                                        </th>
+
+                                        <th
+                                            scope='col'
+                                            className='px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500'
+                                        >
+                                            <span>Assignment Status</span>
+                                        </th>
+
+                                        <th
+                                            scope='col'
+                                            className='px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500'
+                                        >
+                                            <button className='flex items-center gap-x-2'>
+                                                <span>Assignment Marks</span>
+                                            </button>
+                                        </th>
+                                        <th
+                                            scope='col'
+                                            className='px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500'
+                                        >
+                                            Obtained Marks
+                                        </th>
+
+                                        <th className='px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500'>
+                                            Feedback
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody className='bg-white divide-y divide-gray-200 '>
+                                    {submissions.map(submission => (
+                                        <tr key={submission._id}>
+                                            <td className='px-4 py-4 text-sm text-gray-500  whitespace-nowrap'>
+                                                {submission.assignmentTitle}
+                                            </td>
+                                            <td className='px-4 py-4 text-sm text-gray-500  whitespace-nowrap'>
+                                                {submission.status}
+                                            </td>
+                                            <td className='px-4 py-4 text-sm text-gray-500  whitespace-nowrap'>
+                                                100
+                                            </td>
+                                            <td className='px-4 py-4 text-sm text-gray-500  whitespace-nowrap'>
+                                                --
+                                            </td>
+
+                                            {/* <td className='px-4 py-4 text-sm whitespace-nowrap'>
+                                                <div className='flex items-center gap-x-6'>
+                                                    <button
+                                                        className='text-gray-500 transition-colors duration-200   hover:text-red-500 focus:outline-none'
+                                                    >
+                                                        <svg
+                                                            xmlns='http://www.w3.org/2000/svg'
+                                                            fill='none'
+                                                            viewBox='0 0 24 24'
+                                                            strokeWidth='1.5'
+                                                            stroke='currentColor'
+                                                            className='w-5 h-5'
+                                                        >
+                                                            <path
+                                                                strokeLinecap='round'
+                                                                strokeLinejoin='round'
+                                                                d='M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0'
+                                                            />
+                                                        </svg>
+                                                    </button>
+
+                                                    <Link
+                                                        to={`/update/${submission._id}`}
+                                                        className='text-gray-500 transition-colors duration-200   hover:text-yellow-500 focus:outline-none'
+                                                    >
+                                                        <svg
+                                                            xmlns='http://www.w3.org/2000/svg'
+                                                            fill='none'
+                                                            viewBox='0 0 24 24'
+                                                            strokeWidth='1.5'
+                                                            stroke='currentColor'
+                                                            className='w-5 h-5'
+                                                        >
+                                                            <path
+                                                                strokeLinecap='round'
+                                                                strokeLinejoin='round'
+                                                                d='M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10'
+                                                            />
+                                                        </svg>
+                                                    </Link>
+                                                </div>
+                                            </td> */}
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
